b93360e93fdd1dc996f288f03dc2ceba698ec3ca
xrueiii <b11705043@g.ntu.edu.tw>
2024-05-27 18:00:22 +0800

    prettier

--- src/app/_components/HeaderBar.tsx
+++ src/app/_components/HeaderBar.tsx
@@ -26,30 +26,56 @@ export default function HeaderBar() {
     route.push(`/contact`);
   };
 
-    return (
-        <div className="bg-amber-100 w-screen flex justify-center fixed top-8 z-50">
-            <div className="w-2/3 justify-between flex">
-                <button className={cn("hover:shadow-md hover:bg-amber-200 py-4 w-1/6", clicked === 1 && "bg-yellow-500 text-white")} onClick={handleClickHomePage}>
-                    <h1 className="lg:text-xl text-sm">
-                        首頁
-                    </h1>
-                </button>
-                <button className={cn("hover:shadow-md hover:bg-amber-200 py-4 w-1/6", clicked === 2 && "bg-yellow-500 text-white")} onClick={handleClickIntroductionPage}>
-                    <h1 className="lg:text-xl text-sm">
-                        練團室介紹
-                    </h1>
-                </button>
-                <button className={cn("hover:shadow-md hover:bg-amber-200 py-4 w-1/6", clicked === 3 && "bg-yellow-500 text-white")} onClick={handleClickReservationPage}>
-                    <h1 className="lg:text-xl text-sm">
-                        練團室預約
-                    </h1>
-                </button>
-                <button className={cn("hover:shadow-md hover:bg-amber-200 py-4 w-1/6", clicked === 4 && "bg-yellow-500 text-white")} onClick={handleClickContactPage}>
-                    <h1 className="lg:text-xl text-sm">
-                        聯絡我們
-                    </h1>
-                </button>
-            </div>
+  return (
+    <div className="flex justify-center items-center">
+      <div className="bg-white shadow-md w-2/3 flex justify-center items-center fixed top-8 z-50 rounded-full">
+        <div className="w-2/3 h-14 flex justify-between items-center py-4 font-semibold">
+          <button
+            className={cn(
+              "transition-all duration-300 ease-in-out py-3 px-4 rounded-xl",
+              clicked === 1
+                ? "bg-[#FFE900] text-black"
+                : "text-gray-700 hover:bg-gray-200"
+            )}
+            onClick={handleClickHomePage}
+          >
+            <h1 className="lg:text-lg text-sm">首頁</h1>
+          </button>
+          <button
+            className={cn(
+              "transition-all duration-300 ease-in-out py-3 px-4 rounded-md",
+              clicked === 2
+                ? "bg-[#FFE900] text-black"
+                : "text-gray-700 hover:bg-gray-200"
+            )}
+            onClick={handleClickIntroductionPage}
+          >
+            <h1 className="lg:text-lg text-sm">練團室介紹</h1>
+          </button>
+          <button
+            className={cn(
+              "transition-all duration-300 ease-in-out py-3 px-4 rounded-md",
+              clicked === 3
+                ? "bg-[#FFE900] text-black"
+                : "text-gray-700 hover:bg-gray-200"
+            )}
+            onClick={handleClickReservationPage}
+          >
+            <h1 className="lg:text-lg text-sm">練團室預約</h1>
+          </button>
+          <button
+            className={cn(
+              "transition-all duration-300 ease-in-out py-3 px-4 rounded-md",
+              clicked === 4
+                ? "bg-[#FFE900] text-black"
+                : "text-gray-700 hover:bg-gray-200"
+            )}
+            onClick={handleClickContactPage}
+          >
+            <h1 className="lg:text-lg text-sm">聯絡我們</h1>
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
