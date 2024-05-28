06f61e797ddd527cf147dd5fbb20444de1b29b79
HankWu37878 <hankwu1222@gmail.com>
2024-05-29 01:29:45 +0800

    headerbar modification

--- src/app/_components/HeaderBar.tsx
+++ src/app/_components/HeaderBar.tsx
@@ -33,11 +33,11 @@ export default function HeaderBar() {
         alt="Your Logo"
         className="fixed top-4 left-8 w-36 h-12 lg:w-60 lg:h-20 z-50 p-2"
       />
-      <div className="bg-white shadow-md w-full max-w-2xl flex justify-center items-center fixed top-8 z-50 rounded-full px-4 lg:px-8">
+      <div className="bg-white shadow-md w-full max-w-4xl flex justify-center items-center fixed top-8 z-50 rounded-full px-4 lg:px-8">
         <div className="w-full h-14 flex justify-between items-center py-4 font-semibold">
           <button
             className={cn(
-              "transition-all duration-300 ease-in-out py-2 px-3 lg:py-3 lg:px-4 rounded-xl",
+              "transition-all duration-300 ease-in-out py-2 px-3 lg:py-3 lg:px-4 rounded-xl w-1/5",
               clicked === 1
                 ? "bg-[#FFE900] text-black"
                 : "text-gray-700 hover:bg-gray-200"
@@ -48,7 +48,7 @@ export default function HeaderBar() {
           </button>
           <button
             className={cn(
-              "transition-all duration-300 ease-in-out py-2 px-3 lg:py-3 lg:px-4 rounded-xl",
+              "transition-all duration-300 ease-in-out py-2 px-3 lg:py-3 lg:px-4 rounded-xl w-1/5",
               clicked === 2
                 ? "bg-[#FFE900] text-black"
                 : "text-gray-700 hover:bg-gray-200"
@@ -59,7 +59,7 @@ export default function HeaderBar() {
           </button>
           <button
             className={cn(
-              "transition-all duration-300 ease-in-out py-2 px-3 lg:py-3 lg:px-4 rounded-xl",
+              "transition-all duration-300 ease-in-out py-2 px-3 lg:py-3 lg:px-4 rounded-xl w-1/5",
               clicked === 3
                 ? "bg-[#FFE900] text-black"
                 : "text-gray-700 hover:bg-gray-200"
@@ -70,7 +70,7 @@ export default function HeaderBar() {
           </button>
           <button
             className={cn(
-              "transition-all duration-300 ease-in-out py-2 px-3 lg:py-3 lg:px-4 rounded-xl",
+              "transition-all duration-300 ease-in-out py-2 px-3 lg:py-3 lg:px-4 rounded-xl w-1/5",
               clicked === 4
                 ? "bg-[#FFE900] text-black"
                 : "text-gray-700 hover:bg-gray-200"
